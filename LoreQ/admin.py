0a3c0ae1baebdaa2bbd72f18e4a4b12494036a54
KalaMapraw <61613866+Thonkla@users.noreply.github.com>
2023-12-03 14:51:04 +0700

    Create admin.py

---
+++ LoreQ/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from LoreQ.models import *
+
+# Register your models here.
+admin.site.register(MENU)
+admin.site.register(USERTABLE)
+admin.site.register(USERNAME)
+admin.site.register(USERQUEUE)
+admin.site.register(USERQUEUE_DETAIL)
